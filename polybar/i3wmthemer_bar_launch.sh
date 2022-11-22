d7606875222030ec1bdb8d463b2b355c2b0f8476
Piotr Romanowski <piotrromanowski1@gmail.com>
2022-11-22 17:05:33 -0500

    Add polybar config from laptop

---
+++ polybar/i3wmthemer_bar_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/env sh
+
+pkill polybar
+
+#sleep 1;
+#
+#polybar i3wmthemer_bar &
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload i3wmthemer_bar &
+  done
+else
+  #polybar --reload example &
+  polybar --reload i3wmthemer_bar &
+fi
